e6a78eff89c8d871208b15d56939f1fc3a48875b
Lucas Stoque <stoque@live.com>
2018-05-12 14:51:55 -0300

    Add address-list component

---
+++ src/components/Address/AddressList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="address-list">
+    <transition-group name="fade">
+      <li
+        class="item"
+        :key="index"
+        v-for="(address, index) in addressList"
+      >
+        <address-card :address="address" :index="index"/>
+      </li>
+    </transition-group>
+  </ul>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import AddressCard from './AddressCard'
+
+export default {
+  components: {
+    AddressCard
+  },
+  computed: {
+    ...mapState('Address', ['addressList'])
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.address-list
+  display: flex
+  flex-wrap: wrap
+  flex-direction: column
+  justify-content: space-between
+  flex-grow: 1
+
+  .item
+    width: 100%
+    margin: 0 0 20px
+</style>
